b8c51020e71eba8c0d273189bc6aec7b9f0e571d
Heronal <antoine.lasserre@2024.icam.fr>
2022-04-06 14:45:47 +0200

    Recommandation avancée

--- Sections/Pages/Recommandation/Backend/recommandation.php
+++ Sections/Pages/Recommandation/Backend/recommandation.php
@@ -1,12 +1,15 @@
 <?php
 $imageUrlProduits = "Sections/Pages/products/img/";
+$imageUrlVNB = "Sections/Pages/Recommandation/img/logoVNB.png";
 require('../../../Layouts/navbar.php');
 
 ?>
 
 <h1> Nos bons plans ! </h1>
 
-    <div>
+    <div><p> Venez dès à présent visiter les lieux que nous avons aimés !</p>
+
+        <img src="<?php echo $imageUrlVNB?>">
 
 
 
