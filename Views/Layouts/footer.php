3f991c5620f494ad2ee5f7cd46fdf6952ff4de22
Heronal <antoine.lasserre@2024.icam.fr>
2022-04-06 17:32:19 +0200

    commit footer

--- Views/Layouts/footer.php
+++ Views/Layouts/footer.php
@@ -25,23 +25,20 @@
                 <ul>
                     <li><a href="#propos">A propos</a></li>
                     <li><a href="../Pages/products/produits.php">Nos produits</a></li>
-                    <li><a href="#contacts">Nous contacter</a></li>
+                    <li><a href="../Pages/Contact/contact.php">Nous contacter</a></li>
                 </ul>
             </div>
             <div class="footer-col">
                 <h4>Contacts</h4>
                 <ul>
-                    <li><a href="#">bar.toulouse@icam.fr</a></li>
-                    <li><a href="#">75 avenue de Grande Bretagne, 31300 Toulouse</a></li>
+                    <li><a href="https://www.icam.fr/nos-campus/icam-toulouse/">Icam Site de Toulouse</a></li>
+                    <li><a href="https://www.google.fr/search?q=icam+toulouse&source=hp&ei=xK9NYvPVGaWMlwTo553AAQ&iflsig=AHkkrS4AAAAAYk291KhczhY8k_P6fNFIJSeDKbRVn5Cd&gs_ssp=eJzj4tZP1zcsSTEuyYo3MGC0UjWoMDRKTE1KMkhLNbI0MTQwMbYyqEi2NDezNE80SkxKTUoxSTX34s1MTsxVKMkvzckvLU4FAIhIFEg&oq=icam+&gs_lcp=Cgdnd3Mtd2l6EAMYADIOCC4QgAQQsQMQxwEQrwEyBQgAEIAEMgUIABCABDILCC4QgAQQxwEQrwEyBQgAEIAEMgUIABCABDILCC4QgAQQxwEQrwEyBQgAEIAEMgUIABCABDIECAAQAzoLCAAQgAQQsQMQgwE6DgguEIAEELEDEMcBEKMCOggIABCABBCxAzoLCC4QgAQQsQMQgwE6DgguEIAEELEDEIMBENQCOggILhCABBCxAzoLCC4QgAQQsQMQ1AI6BQgAELEDUABYsAdgyhFoAHAAeACAAXaIAa0EkgEDMS40mAEAoAEB&sclient=gws-wiz">75 avenue de Grande Bretagne, 31300 Toulouse</a></li>
 
                 </ul>
             </div>
             <div class="footer-col">
                 <h4>Documentations</h4>
                 <ul >
-                    <!-- <li><a href="PapiersAssociation/Statuts.pdf">L'association</a></li>
-                    <li><a href="PapiersAssociation/R.pdf">Réglement intérieur</a></li>
-                    <li><a href="https://lydia-app.com/collect/17895-les-poubelles-rouges/fr">La cagnotte</a></li> -->
                     <li><a href="https://fr-fr.facebook.com/pages/category/Cleaning-Service/SAS-Nettysol-1692848397395910/">Document publicitaire</a></li>
 
                 </ul>
