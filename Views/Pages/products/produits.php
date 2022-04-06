f3dc59f33f1a15e5da6f7182175bc77fe781b62c
Heronal <antoine.lasserre@2024.icam.fr>
2022-04-06 16:45:05 +0200

    Refactor scss

--- Views/Pages/products/produits.php
+++ Views/Pages/products/produits.php
@@ -5,7 +5,7 @@ $imageUrlProduits = "Sections/Pages/products/img/";
 ?>
 
 
-<link rel="stylesheet" type="text/css" href="siteBAR/Views/Pages/products/produits.scss">
+<link rel="stylesheet" type="text/css" href="../../CSS-Global/CSS/style.css">
 <!-- <section class="produitBiere" id="produitBiere">
  -->
 <div class="produitBiere">
