d4d9175191b6f43d45359d506f4d6de1d01148b8
Heronal <antoine.lasserre@2024.icam.fr>
2022-04-06 15:42:33 +0200

    Recommandation update

--- Views/Pages/Recommandation/Backend/recommandation.php
+++ Views/Pages/Recommandation/Backend/recommandation.php
@@ -1,6 +1,6 @@
 <?php
 $imageUrlProduits = "Sections/Pages/products/img/";
-$imageUrlVNB = "Sections/Pages/Recommandation/img/";
+$imageUrlVNB = "../../img/";
 require('../../../Layouts/navbar.php');
 
 ?>
@@ -10,7 +10,11 @@ require('../../../Layouts/navbar.php');
     <div><p> Venez dès à présent visiter les lieux que nous avons aimés !</p>
 
         <img src="<?php echo $imageUrlVNB?>logoVNB.png" alt=" ">
-
+        <p>La boutique V&B de purpan est l'un de nos meilleurs bons plans, située tout près de l'ICAM, certains la reconnaîtront sûrement. De nombreuses bières y sont proposées dans un cadre chaleureux et bon enfant.
+            De plus, il proposent également certains des meilleurs vins du sud-ouest pour le plaisir de vos papilles ! En tout cas nous on adore !</p>
+        <img src="<?php echo $imageUrlVNB?>3brasseurs.png" alt=" ">
+        <p>Dans un tout autre style, les 3 Brasseurs de Labège proposent le charme d'un bistrot couplé à de délicieuses bières.
+            Le choix est large et la qualité de la bière comme de la nourriture sont toujours au rendez-vous. On vous recommande fortement d'aller tester tout ça !</p>
 
     </div>
 
