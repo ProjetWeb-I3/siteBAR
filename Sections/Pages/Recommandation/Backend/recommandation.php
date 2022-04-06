c0b7fdf39a8713d5fe10abcdb38650f2c0c4b7f1
Heronal <antoine.lasserre@2024.icam.fr>
2022-04-06 15:00:20 +0200

    Recommandation avancée + unversioned files css

--- Sections/Pages/Recommandation/Backend/recommandation.php
+++ Sections/Pages/Recommandation/Backend/recommandation.php
@@ -1,6 +1,6 @@
 <?php
 $imageUrlProduits = "Sections/Pages/products/img/";
-$imageUrlVNB = "Sections/Pages/Recommandation/img/logoVNB.png";
+$imageUrlVNB = "Sections/Pages/Recommandation/img/";
 require('../../../Layouts/navbar.php');
 
 ?>
@@ -9,8 +9,7 @@ require('../../../Layouts/navbar.php');
 
     <div><p> Venez dès à présent visiter les lieux que nous avons aimés !</p>
 
-        <img src="<?php echo $imageUrlVNB?>">
-
+        <img src="<?php echo $imageUrlVNB?>logoVNB.png" alt=" ">
 
 
     </div>
