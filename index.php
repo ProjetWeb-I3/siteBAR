df002550f1ac15780f607f10b9c6567ce7def90b
Heronal <antoine.lasserre@2024.icam.fr>
2022-05-26 11:29:32 +0200

    product update final

--- index.php
+++ index.php
@@ -30,12 +30,9 @@ require $_SERVER['DOCUMENT_ROOT'] . '/Views/Layouts/navbar.php';
 </section>
 
 <section>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+    <p class="p-accueil">Bienvenue sur le site officiel du bar de l'Icam Toulouse ! Sur ce site vous pourrez découvrir nos délicieuses bières,
+    et nos adresses préférées ! N'oubliez pas de nous rejoindre sur nos réseaux et n'hésitez pas à nous contacter pour toute
+    demande ! </p>
 </section>
 
 <?php require('Views/Layouts/footer.php'); ?>
