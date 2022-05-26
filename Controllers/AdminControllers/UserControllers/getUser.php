36f3d9fc6b1af3033cf19fee3a14e629372ccee6
Killian Larcher <killian.larcher@2024.icam.fr>
2022-05-26 15:58:47 +0200

    modifying admin space final 3

--- Controllers/AdminControllers/UserControllers/getUser.php
+++ Controllers/AdminControllers/UserControllers/getUser.php
@@ -4,13 +4,13 @@ function getMembre($conn, $nb = null, $id = null)
 {
 
     if ($nb and !$id) {
-        $req = $conn->query('SELECT * FROM membres LIMIT' . $nb);
+        $req = $conn->query('SELECT * FROM users LIMIT' . $nb);
         $membre = $req->fetchAll();
     } elseif ($id) {
-        $req = $conn->query('SELECT * FROM membres WHERE id =' . $id);
+        $req = $conn->query('SELECT * FROM users WHERE id_users =' . $id);
         $membre = $req->fetchObject();
     } else {
-        $req = $conn->query('SELECT * FROM membres ');
+        $req = $conn->query('SELECT * FROM users ');
         $membre = $req->fetchAll();
     }
     return $membre;
