436de71e7968c25383848a431427c880495b88e4
GAUTHEREAU <charlesgte31@gmail.com>
2022-05-26 12:26:05 +0200

    cart functions

--- Views/Pages/Cart/functionADDCart.php
+++ Views/Pages/Cart/functionADDCart.php
@@ -3,37 +3,40 @@
 <?php
 
 session_start();
-require_once 'connect.php';
+require_once '../../../Models/DatabaseModel/connect.php';
+global $conn;
 
 
 
-if (isset($_SESSION['id_user']) AND !empty($_SESSION['id_user']))
+if (isset($_SESSION['id_users']) AND !empty($_SESSION['id_users']))
 
 {	
 	if (isset($_GET['refProductCart'])) 
 	{
-		$ref= $_GET['refProductCart'] ; 
-		$user = $_SESSION['id_user'];
-		$req = $conn->prepare('SELECT * FROM Carts WHERE refProductCart =? AND UserIdCart = ?');
+		$ref= $_GET['refProductCart'] ;
+        echo $_GET['refProductCart'];
+		$user = $_SESSION['id_users'];
+		$req = $conn->prepare('SELECT * FROM carts WHERE id_products =? AND id_users = ?');
 		$req->execute(array($ref,$user));
 		$ProductExistCart = $req->rowCount();
+
 		
 		if ($ProductExistCart == 0)
 		{
 			
 			$qtyProduct= "1";
-			$req2 = $conn->query("INSERT INTO Carts (refProductCart, UserIdCart, quantity_product) VALUES ('$ref','$user' ,'$qtyProduct')");
-			header('location:produits.php');
+			$req2 = $conn->query("INSERT INTO carts (id_products, id_users, quantity_carts) VALUES ('$ref','$user','$qtyProduct')");
+			header('location:../products/products.php');
 		}
 		else
 		{
 			
-			$req3 = $conn->query("SELECT * FROM Carts WHERE refProductCart = '$ref' AND UserIdCart = '$user' ");
+			$req3 = $conn->query("SELECT * FROM carts WHERE id_products = '$ref' AND id_users = '$user' ");
 			
 			$request = $req3->fetch();
-			$QTY = $request['quantity_product'] + 1;
-			$req4 = $conn->exec("UPDATE Carts SET quantity_product = '$QTY' WHERE refProductCart = '$ref' AND UserIdCart = '$user' ");
-			header('location:produits.php');
+			$QTY = $request['quantity_carts'] + 1;
+			$req4 = $conn->exec("UPDATE carts SET quantity_carts = '$QTY' WHERE id_products = '$ref' AND id_users = '$user' ");
+			header('location:../products/products.php');
 
 			
 		}
