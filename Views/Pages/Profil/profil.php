dccddf4c9830084413b1246fa8419c97dd4649bb
Dorian-LB <dorian.le-boulch@2024.icam.fr>
2022-05-27 11:25:41 +0200

    infos page profil récupérées

--- Views/Pages/Profil/profil.php
+++ Views/Pages/Profil/profil.php
@@ -1,2 +1,17 @@
 <?php
-echo "page profil";
+session_start();
+global $conn;
+require_once '../../../Models/DatabaseModel/connect.php';
+$getid = $_SESSION['id_users'];
+$requser = $conn->prepare('SELECT * FROM users WHERE id_users = ?');
+$requser->execute(array($getid));
+$userinfo = $requser->fetch();
+
+echo "page profil<br>
+Bienvenue ".$userinfo['username_users']." sur le site officiel du bar de L'Icam de Toulouse<br>
+Vos informations :<br>
+Nom : ".$userinfo['lastname_users']."<br>
+prénom : ".$userinfo['firstname_users']."<br>
+pseudo : ".$userinfo['username_users']."<br>
+Promotion : ".$userinfo['promotion_users']."<br>";
+?>
